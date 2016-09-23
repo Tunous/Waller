2fcd63d8cdf13d36848ec2f7be9b3562df6c102d
Tunous <lukus178@gmail.com>
2016-09-23 17:07:44 +0200

    Create preferences window with Glade

--- prefs.js
+++ prefs.js
@@ -12,9 +12,9 @@ function init() {
 function buildPrefsWidget() {
     let buildable = new Gtk.Builder();
     buildable.add_from_file(Me.dir.get_path() + '/prefs.xml');
-    let box = buildable.get_object('scrolled_window_built');
+    let box = buildable.get_object('window');
 
-    settings.bind('show-panel-icon', buildable.get_object('field_show_panel_icon'), 'active', Gio.SettingsBindFlags.DEFAULT);
+    settings.bind('show-panel-icon', buildable.get_object('showPanelIconSwitch'), 'active', Gio.SettingsBindFlags.DEFAULT);
 
     box.show_all();
     return box;
